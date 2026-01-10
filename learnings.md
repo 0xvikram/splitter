80d626a93828ca1d80f66849be119d2839ed6d35
0xvikram <vsvikramsingh305@gmail.com>
2026-01-11 01:26:10 +0530

    initilaized the project and wrote common.ts file

---
+++ learnings.md
@@ -0,0 +1,2 @@
+/types
+it onlyincludes what types of data exists and how it is shaped
---
+++ types/common.ts
@@ -0,0 +1,40 @@
+// I have to put those things here which are used commonly excluidng all the functionalties and i declare them once in this file and no need to make chnages here again and again
+
+// import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
+
+// export type currency=string;
+// export type money=number;
+
+// export type supportedCurrency="INR";
+// //date when the group was created 
+// export type groupCreatedAt=string;
+// export type transactionDate=string;
+// //states group is active or not 
+// export type groupStatus=boolean;
+
+// export type addedExpenseId=number;
+// export type clearedExpenseId=number;
+// export type groupId=number;
+// //this the transaction id that is listed wheather you add or clear a expense in group
+// export type everytransactionId=number;
+// export type groupMembers=string;
+
+
+export type supportedCurrency="INR";
+export type money=number;
+
+
+export type Id=number;
+export type createdAt=number;
+// export interface users{
+// name:string;
+// groupjoined:number;
+
+// }
+
+export type groupStatus="Active"|"Dead";
+
+
+
+
+
