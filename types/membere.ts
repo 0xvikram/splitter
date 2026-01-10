72fd6649cd15f117b8ff3e2cd669672c6e898f41
0xvikram <vsvikramsingh305@gmail.com>
2026-01-11 01:40:12 +0530

    wrote member.ts  file

---
+++ types/membere.ts
@@ -0,0 +1,15 @@
+//this file code tells us what data represents a participant inside a group
+
+import {Id,money,createdAt} from "./common";
+
+export interface Member{
+    memberId:Id;
+    isActive:boolean;
+  
+    joinedAt:createdAt;
+    name:string;
+    moneyOwed:money;
+    moneyLent:money;
+
+
+}
